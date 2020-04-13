f9f29fde7b35e6ebf5783299e7934dfbf5938b0e
Natalie Estrada <nmestrad@gmail.com>
2020-04-13 14:04:17 -0500

    added materializeCSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import 'materialize-css';
+import { Card, CardTitle } from 'react-materialize';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -19,17 +21,21 @@ function App() {
   return (
     <div className="app">
        <nav>
-           <p>Simple Nav Bar Title</p>
-           <a href="https://reactjs.org">link</a>
-           <a href="https://reactjs.org">link</a>
-           <a href="https://reactjs.org">link</a>
+           <div className="nav-wrapper">
+                <a href="https://reactjs.org" className="brand-logo">Nav Bar Title</a>
+                <ul className="right hide-on-med-and-down">
+                    <li><a href="https://reactjs.org">link</a></li>
+                    <li><a href="https://reactjs.org">link</a></li>
+                    <li><a href="https://reactjs.org">link</a></li>
+                </ul>
+           </div>
        </nav>
         <div className="body">
-            <h2>Todos List</h2>
-            <div>
-                {todos.map(todo => <div key={todo.id}>
-                <p>{todo.title}</p>
-                </div>)}
+            <h2 className="center-align">Todos List</h2>
+            <div className="container">
+                {todos.map(todo => <Card key={todo.id} className="collapsible">
+                <CardTitle className="black-text collapsible-header">{todo.title}</CardTitle>
+                </Card>)}
             </div>
         </div>
     </div>
